64f9e11de6593138def2933bb7c8cc413c35c764
Mariia Kulikova <tinky-winky@MariiasAir1668.fritz.box>
2025-03-23 19:34:20 +0100

    Remove deprecated Docusaurus configuration and plugin files

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/tinky-winky/projects/portfolio/mashku/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/tinky-winky/projects/portfolio/mashku/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/tinky-winky/projects/portfolio/mashku/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/tinky-winky/projects/portfolio/mashku/src/css/custom.css"),
-];
