b7719da95bd0083aa9c58239f41b2ed015b17a23
Mariia Kulikova <tinky-winky@MariiasAir1668.fritz.box>
2025-06-10 00:55:38 +0200

    feat: refine footer layout and update contact links; adjust button styles and typography

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,22 +5,21 @@ import buttonStyles from '../button/button.module.css';
 export default function Footer() {
   return (
     <section className={styles.contacts}>
+      <div><h2>Contacts</h2></div>
       <div className={styles.contactsContent}>
         <div className={styles.leftColumn}>
-          <h1 className={styles.contactsH1}>
+          <h3>
             I'm always open to new connections - if you have an interesting project or just want to chat, feel free to reach out!
-          </h1>
-        </div>
-        <div className={styles.rightColumn}>
+          </h3>
           <div className={styles.contactsSocials}>
-            <Link className={`${buttonStyles['button-text']} ${styles.socialLink}`} to="https://www.instagram.com/mashku.me">
-              <span>Instagram</span>
-            </Link>
-            <Link className={`${buttonStyles['button-text']} ${styles.socialLink}`} to="https://medium.com/@uxoose">
-              <span>Medium</span>
+            <Link className={`${buttonStyles['button-text']} ${styles.socialLink}`} to="mailto:mariakulikova18.01@gmail.com">
+              <span>Email</span>
             </Link>
             <Link className={`${buttonStyles['button-text']} ${styles.socialLink}`} to="https://t.me/mashku_me">
               <span>Telegram</span>
+            </Link>
+             <Link className={`${buttonStyles['button-text']} ${styles.socialLink}`} to="https://www.instagram.com/mashku.me">
+              <span>Instagram</span>
             </Link>
             <Link className={`${buttonStyles['button-text']} ${styles.socialLink}`} to="https://dribbble.com/mashku">
               <span>Dribbble</span>
